cf8a358248d29344926477dc22a8a07bd169e9ca
Florent Thiéry <florent.thiery@ubicast.eu>
2016-11-16 23:35:22 +0100

    keep hostname prefix only

--- utils/time.py
+++ utils/time.py
@@ -22,7 +22,7 @@ def time_took_ms(function):
 
 def get_timestamped_fname():
     ts = time.strftime('%Y-%m-%d-%H-%M')
-    fname = 'result-%s-%s.txt' % (socket.gethostname(), ts)
+    fname = 'result-%s-%s.txt' % (socket.gethostname().split('.')[0], ts)
     if os.path.exists(fname):
         fname += '-1' 
     return fname
